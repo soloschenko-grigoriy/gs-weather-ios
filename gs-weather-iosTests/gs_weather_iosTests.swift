8dc17e68bd73fe7fbe2314a65c87c9238f2489c3
Gregory Soloshchenko <gregory.soloshchenko@wipro.com>
2018-04-18 11:33:10 -0400

    Initial Commit

---
+++ gs-weather-iosTests/gs_weather_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  gs_weather_iosTests.swift
+//  gs-weather-iosTests
+//
+//  Created by Gregory Soloshchenko on 4/18/18.
+//  Copyright © 2018 Gregory Soloshchenko. All rights reserved.
+//
+
+import XCTest
+@testable import gs_weather_ios
+
+class gs_weather_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
